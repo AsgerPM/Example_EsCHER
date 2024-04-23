0f6af1fb797e556beeae1a6f1ffe52a03a2089d5
AsgerPM <128593484+AsgerPM@users.noreply.github.com>
2024-04-23 14:50:48 +0200

    Updating documentation of function

--- R/penguin_plot.R
+++ R/penguin_plot.R
@@ -1,7 +1,7 @@
 #' Penguin Plot Histogram
 #' A function to create a histogram for each species based on specified numeric variable
 #' @param data A data frame containing information about different species, with at least one numeric variable and a variable named species to group by
-#' @param variable_name A numeric variable to plot
+#' @param variable_name A numeric variable to plot, the variable name has to be a string
 #' @return A histogram of the containing the summary variables (count, arithmetic mean, standard deviation, minimum and maximum value) for the chosen variable
 #' @examples
 #' Example usage:
--- R/summarize_species.R
+++ R/summarize_species.R
@@ -3,12 +3,7 @@
 #' Summarise the count, arithmetic mean, standard deviation, and minimum and maximum values for a specified numeric variable in a dataframe dataset of species
 #' @param data A data frame containing information about different species, with at least one numeric variable and a variable named species to group by
 #' @param variable A numeric variable to summarise
-#' @return A histogram of the containing the summary variables (count, arithmetic mean, standard deviation, minimum and maximum value) for the chosen variable
-#' @examples
-#' Example usage:
-#' summarize_species(data = palmerpenguins::penguins,
-#'                variable = flipper_length_mm)
-#' @export
+.
 
 # Function to calculate summary statistics for a specified numeric variable by species
 summarize_species <- function(data, variable) {
@@ -28,5 +23,5 @@ summarize_species <- function(data, variable) {
 }
 
 # Example usage:
-summarize_species(data = palmerpenguins::penguins,
-                  variable = flipper_length_mm)
+# summarize_species(data = palmerpenguins::penguins,
+#                  variable = flipper_length_mm)
